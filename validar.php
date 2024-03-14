274c9792b6ef0f8255dcc9fc48582dc2194bc84f
Luis Leal <luisrrleal@gmail.com>
2024-03-14 10:32:58 -0600

    first commit

---
+++ validar.php
@@ -0,0 +1,11 @@
+<?php
+// Recibir los datos del formulario, revisar en base de datos que concuerden y redirigir al lugar pertinente
+
+// Obtenemos el correo y lo guardamos en una variable
+$correo = $_POST['mail'];
+// Obtenemos la contraseña y la guardamos en una variable
+$_POST['password'];
+
+
+echo $correo;
+echo "Ya llegueee!";
