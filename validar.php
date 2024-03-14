2bdf84f88698f58d8522709b7fac477df00172ec
Luis Leal <luisrrleal@gmail.com>
2024-03-14 11:00:24 -0600

    Start adding the conection to sql and working with php

--- validar.php
+++ validar.php
@@ -4,8 +4,28 @@
 // Obtenemos el correo y lo guardamos en una variable
 $correo = $_POST['mail'];
 // Obtenemos la contraseña y la guardamos en una variable
-$_POST['password'];
+$pass = $_POST['password'];
 
 
-echo $correo;
+// Conectamos a la base de datos
+// El uso de la base de datos está dividido en tres pasos
+
+// Paso 1: Abrir la conexión
+$con = mysqli_connect('localhost', 'basta', '1234', 'basta');
+
+if ($con == null) exit;
+else {
+    // Paso 2: Procesar la consulta
+    // 2.1 Realizar la consulta
+    $query = "select * from usuario where correo = '$correo' and clave = '$pass'";
+    echo $query;
+    $usuario = mysqli_query($con, $query);
+    // 2.2 Procesar el resultado
+
+}
+
+
+// Paso 3: Cerrar la conexión
+mysqli_close($con);
+
 echo "Ya llegueee!";
